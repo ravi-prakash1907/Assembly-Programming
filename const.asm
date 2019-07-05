5e170a1348c38fffb40c858e04e7e0592af45b5d
ravi-prakash1907 <1907prakash.ravi@gmail.com>
2019-07-05 17:20:26 +0530

    fix

--- const.asm
+++ const.asm
@@ -1,10 +1,9 @@
 ; Use of CONSTANTS  < CONSTANT_NAME EQU expression >
 
 ; Tyoes:
-  ; EQU
-  ; %assign
-  ; %define
-
+  ; EQU     :  assigns numeric constants
+  ; %assign :  assigns numeric constants
+  ; %define :  assigns both numeric and string constants like #define
 
 
 ; they are need noot to be decleared in the data section
@@ -13,11 +12,17 @@ SYS_WRITE equ 4
 STDIN     equ 0
 STDOUT    equ 1
 
+; Also:-
+; %assign  TOTAL  20
+; %define PTR [EBP+4]   ; replaces all PRT with EBP+4
+
 
 section .text
   global _start
 
 _start:
+
+  ; EQU
   mov  eax,  SYS_WRITE
   mov  ebx,  STDOUT
   mov  ecx,  msg1
@@ -40,6 +45,7 @@ _start:
   int  0x80
 
 
+
 section	 .data
   msg1 db	'Hello, programmers!',0xa,0xd   ; 0xd = carriage return ('\r'), Moves cursor at beg. of current row
   len1 equ $ - msg1
@@ -49,3 +55,5 @@ section	 .data
 
   msg3 db 'Linux assembly programming! ',0xa,0xd
   len3 equ $- msg3
+
+  s2 times 9 db '*'
